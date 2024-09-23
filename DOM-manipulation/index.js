83acadec62f613d7e9bf16363383c9d50ad4b379
gcabashelindelihle4@gmail.com <gcabashelindelihle4@gmail.com>
2024-09-23 21:57:44 +0200

    added dom manipulation

---
+++ DOM-manipulation/index.js
@@ -0,0 +1,7 @@
+console.log(document.URL)
+console.log(document.title)
+// document.title= 123
+// console.log(document.title)
+console.log(document.doctype)
+console.log(document.head)
+console.log(document.all)
